inconsiderable/focal-point

adapt cruzbit for staging continuity

irc.go
@@ -1,0 +1,113 @@
+package focalpoint
+
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"encoding/hex"
+	"io"
+	"log"
+	mrand "math/rand"
+	"regexp"
+	"strconv"
+	"sync"
+
+	irc "github.com/thoj/go-ircevent"
+)
+
+const Server = "irc.freenode.net:7000"
+
+// IRC is used for bootstrapping the network for now.
+// It primarily exists as a backup to our current limited set of DNS seeders.
+type IRC struct {
+	conn *irc.Connection
+	wg   sync.WaitGroup
+}
+
+// NewIRC returns a new IRC bootstrapper.
+func NewIRC() *IRC {
+	return &IRC{}
+}
+
+// Connect connects the IRC bootstrapper to the IRC network.
+// port is our local focalpoint port. If it's set to 0 we won't be used for inbound connections.
+func (i *IRC) Connect(genesisID CountID, port int, addrChan chan<- string) error {
+	nick, err := generateRandomNick()
+	if err != nil {
+		return err
+	}
+
+	numReg, err := regexp.Compile("[^0-9]+")
+	if err != nil {
+		return err
+	}
+
+	i.conn = irc.IRC(nick, strconv.Itoa(port))
+	i.conn.UseTLS = true
+	i.conn.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	var channel string
+	i.conn.AddCallback("001", func(e *irc.Event) {
+		n := mrand.Intn(10)
+		channel = generateChannelName(genesisID, n)
+		log.Printf("Joining channel %s\n", channel)
+		i.conn.Join(channel)
+	})
+	i.conn.AddCallback("366", func(e *irc.Event) {
+		log.Printf("Joined channel %s\n", channel)
+		i.conn.Who(channel)
+	})
+	i.conn.AddCallback("352", func(e *irc.Event) {
+		if e.Arguments[5] == nick {
+			return
+		}
+		port := numReg.ReplaceAllString(e.Arguments[2], "")
+		host := e.Arguments[3]
+		if len(port) != 0 && port != "0" {
+			peer := host + ":" + port
+			addrChan <- peer
+		}
+	})
+	i.conn.AddCallback("JOIN", func(e *irc.Event) {
+		if e.Nick == nick {
+			return
+		}
+		port := numReg.ReplaceAllString(e.User, "")
+		host := e.Host
+		if len(port) != 0 && port != "0" {
+			peer := host + ":" + port
+			addrChan <- peer
+		}
+	})
+
+	return i.conn.Connect(Server)
+}
+
+// Run executes the IRC bootstrapper's main loop in its own goroutine.
+func (i *IRC) Run() {
+	i.wg.Add(1)
+	go func() {
+		defer i.wg.Done()
+		i.conn.Loop()
+	}()
+}
+
+// Shutdown stops the IRC bootstrapper.
+func (i *IRC) Shutdown() {
+	log.Println("IRC shutting down...")
+	i.conn.Quit()
+	i.wg.Wait()
+	log.Println("IRC shutdown")
+}
+
+func generateRandomNick() (string, error) {
+	nickBytes := make([]byte, 8)
+	if _, err := io.ReadFull(rand.Reader, nickBytes); err != nil {
+		return "", err
+	}
+	return "cb" + hex.EncodeToString(nickBytes)[2:], nil
+}
+
+func generateChannelName(genesisID CountID, n int) string {
+	g := genesisID.String()
+	return "#focalpoint-" + g[len(g)-8:] + "-" + strconv.Itoa(n)
+}

tls.go
@@ -1,0 +1,115 @@
+package focalpoint
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	certLifetime = time.Hour * 24 * 365 // 1 year, but we create a new one every time we startup
+
+	certName = "cert.pem"
+
+	keyName = "key.pem"
+)
+
+// Client config
+var tlsClientConfig *tls.Config = &tls.Config{
+	RootCAs:                  nil,
+	InsecureSkipVerify:       true,
+	MinVersion:               tls.VersionTLS12,
+	CurvePreferences:         []tls.CurveID{tls.CurveP256, tls.CurveP384, tls.CurveP521},
+	PreferServerCipherSuites: false,
+	CipherSuites: []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	},
+}
+
+// Server config
+var tlsServerConfig *tls.Config = &tls.Config{
+	MinVersion:               tls.VersionTLS12,
+	CurvePreferences:         []tls.CurveID{tls.CurveP256, tls.CurveP384, tls.CurveP521},
+	PreferServerCipherSuites: true,
+	CipherSuites: []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	},
+}
+
+// Generate ephemeral x.509 certificate and private key pair. They're written to the -datadir
+func generateSelfSignedCertAndKey(tlsDataDir string) (certPath, keyPath string, err error) {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return "", "", err
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(certLifetime)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return "", "", err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"focalpoint client"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &privKey.PublicKey, privKey)
+	if err != nil {
+		return "", "", err
+	}
+
+	certPath = filepath.Join(tlsDataDir, certName)
+	certOut, err := os.Create(certPath)
+	if err != nil {
+		return "", "", err
+	}
+	defer certOut.Close()
+	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
+		return "", "", err
+	}
+
+	keyPath = filepath.Join(tlsDataDir, keyName)
+	keyOut, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return "", "", err
+	}
+	defer keyOut.Close()
+	keyBytes, err := x509.MarshalECPrivateKey(privKey)
+	if err != nil {
+		return "", "", err
+	}
+	if err := pem.Encode(keyOut, &pem.Block{Type: "EC PRIVATE KEY", Bytes: keyBytes}); err != nil {
+		return "", "", err
+	}
+
+	return certPath, keyPath, nil
+}

necessitated/consequence

irc.go
@@ -1,0 +1,113 @@
+package consequence
+
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"encoding/hex"
+	"io"
+	"log"
+	mrand "math/rand"
+	"regexp"
+	"strconv"
+	"sync"
+
+	irc "github.com/thoj/go-ircevent"
+)
+
+const Server = "irc.freenode.net:7000"
+
+// IRC is used for bootstrapping the network for now.
+// It primarily exists as a backup to our current limited set of DNS seeders.
+type IRC struct {
+	conn *irc.Connection
+	wg   sync.WaitGroup
+}
+
+// NewIRC returns a new IRC bootstrapper.
+func NewIRC() *IRC {
+	return &IRC{}
+}
+
+// Connect connects the IRC bootstrapper to the IRC network.
+// port is our local consequence port. If it's set to 0 we won't be used for inbound connections.
+func (i *IRC) Connect(genesisID StageID, port int, addrChan chan<- string) error {
+	nick, err := generateRandomNick()
+	if err != nil {
+		return err
+	}
+
+	numReg, err := regexp.Compile("[^0-9]+")
+	if err != nil {
+		return err
+	}
+
+	i.conn = irc.IRC(nick, strconv.Itoa(port))
+	i.conn.UseTLS = true
+	i.conn.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	var channel string
+	i.conn.AddCallback("001", func(e *irc.Event) {
+		n := mrand.Intn(10)
+		channel = generateChannelName(genesisID, n)
+		log.Printf("Joining channel %s\n", channel)
+		i.conn.Join(channel)
+	})
+	i.conn.AddCallback("366", func(e *irc.Event) {
+		log.Printf("Joined channel %s\n", channel)
+		i.conn.Who(channel)
+	})
+	i.conn.AddCallback("352", func(e *irc.Event) {
+		if e.Arguments[5] == nick {
+			return
+		}
+		port := numReg.ReplaceAllString(e.Arguments[2], "")
+		host := e.Arguments[3]
+		if len(port) != 0 && port != "0" {
+			peer := host + ":" + port
+			addrChan <- peer
+		}
+	})
+	i.conn.AddCallback("JOIN", func(e *irc.Event) {
+		if e.Nick == nick {
+			return
+		}
+		port := numReg.ReplaceAllString(e.User, "")
+		host := e.Host
+		if len(port) != 0 && port != "0" {
+			peer := host + ":" + port
+			addrChan <- peer
+		}
+	})
+
+	return i.conn.Connect(Server)
+}
+
+// Run executes the IRC bootstrapper's main loop in its own goroutine.
+func (i *IRC) Run() {
+	i.wg.Add(1)
+	go func() {
+		defer i.wg.Done()
+		i.conn.Loop()
+	}()
+}
+
+// Shutdown stops the IRC bootstrapper.
+func (i *IRC) Shutdown() {
+	log.Println("IRC shutting down...")
+	i.conn.Quit()
+	i.wg.Wait()
+	log.Println("IRC shutdown")
+}
+
+func generateRandomNick() (string, error) {
+	nickBytes := make([]byte, 8)
+	if _, err := io.ReadFull(rand.Reader, nickBytes); err != nil {
+		return "", err
+	}
+	return "cb" + hex.EncodeToString(nickBytes)[2:], nil
+}
+
+func generateChannelName(genesisID StageID, n int) string {
+	g := genesisID.String()
+	return "#consequence-" + g[len(g)-8:] + "-" + strconv.Itoa(n)
+}

tls.go
@@ -1,0 +1,115 @@
+package consequence
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	certLifetime = time.Hour * 24 * 365 // 1 year, but we create a new one every time we startup
+
+	certName = "cert.pem"
+
+	keyName = "key.pem"
+)
+
+// Client config
+var tlsClientConfig *tls.Config = &tls.Config{
+	RootCAs:                  nil,
+	InsecureSkipVerify:       true,
+	MinVersion:               tls.VersionTLS12,
+	CurvePreferences:         []tls.CurveID{tls.CurveP256, tls.CurveP384, tls.CurveP521},
+	PreferServerCipherSuites: false,
+	CipherSuites: []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	},
+}
+
+// Server config
+var tlsServerConfig *tls.Config = &tls.Config{
+	MinVersion:               tls.VersionTLS12,
+	CurvePreferences:         []tls.CurveID{tls.CurveP256, tls.CurveP384, tls.CurveP521},
+	PreferServerCipherSuites: true,
+	CipherSuites: []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	},
+}
+
+// Generate ephemeral x.509 certificate and private key pair. They're written to the -datadir
+func generateSelfSignedCertAndKey(tlsDataDir string) (certPath, keyPath string, err error) {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return "", "", err
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(certLifetime)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return "", "", err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"consequence client"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &privKey.PublicKey, privKey)
+	if err != nil {
+		return "", "", err
+	}
+
+	certPath = filepath.Join(tlsDataDir, certName)
+	certOut, err := os.Create(certPath)
+	if err != nil {
+		return "", "", err
+	}
+	defer certOut.Close()
+	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
+		return "", "", err
+	}
+
+	keyPath = filepath.Join(tlsDataDir, keyName)
+	keyOut, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return "", "", err
+	}
+	defer keyOut.Close()
+	keyBytes, err := x509.MarshalECPrivateKey(privKey)
+	if err != nil {
+		return "", "", err
+	}
+	if err := pem.Encode(keyOut, &pem.Block{Type: "EC PRIVATE KEY", Bytes: keyBytes}); err != nil {
+		return "", "", err
+	}
+
+	return certPath, keyPath, nil
+}